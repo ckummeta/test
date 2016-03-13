cda06afaf2bf6b4b8cdf5d41e085ff4854956ece
kummetac <cku02@SKYGO-MacBook.local>
2016-03-13 15:49:35 +0000

    Removing commented lines of code

--- features/support/page_obect/pages/base_page.rb
+++ features/support/page_obect/pages/base_page.rb
@@ -4,8 +4,4 @@ class BasePage < SitePrism::Page
     def navigation_timeout
       Capybara.default_wait_time
     end
-
-    #section :footer, Footer, '#globalFooterNav'
-    #section :header, Header, '.navContainer'
-
   end
